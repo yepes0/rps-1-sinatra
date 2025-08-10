ba9fc3e12dcde17b0f06e38d9397b170b4f9a0ca
yepes0 <71985093+yepes0@users.noreply.github.com>
2025-08-10 03:31:52 +0000

    ok, let's grade it again, i think we're looking pretty goooood

--- app.rb
+++ app.rb
@@ -7,24 +7,22 @@ end
 
 get("/rock") do
   moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample # chooses a rand 
+erb(:rock)
+end
+
+get("/paper") do
+moves = ["rock", "paper", "scissors"]
 
   @comp_move = moves.sample # chooses a rand 
 
 
-
-erb(:zebra)
-
-
-end
-
-get("/paper") do
-"whaddup, paper"
-erb(:zebra)
-
+erb(:paper)
 end
 
 get("/scissors") do
-"whaddup, scissors"
-erb(:zebra)
+moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample # chooses a rand 
+erb(:scissors)
 
 end
