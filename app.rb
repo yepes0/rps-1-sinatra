90c5d61ee915a59a6f57ece81c7d6c21a23474ef
yepes0 <71985093+yepes0@users.noreply.github.com>
2025-08-10 02:43:24 +0000

    before checking my grade... changuitos!

--- app.rb
+++ app.rb
@@ -2,8 +2,29 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample # chooses a rand 
+
+
+
+erb(:zebra)
+
+
+end
+
+get("/paper") do
+"whaddup, paper"
+erb(:zebra)
+
+end
+
+get("/scissors") do
+"whaddup, scissors"
+erb(:zebra)
+
 end
